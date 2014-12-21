47691d4d133b973cac879e6ef257fb25df413ddf
kzk0920 <kzk0920@gmail.com>
2014-12-21 17:34:33 +0900

    Add animation.

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(void)
 {
-	printf("Hello, world!\n");
+	char *str = "Hello, world!";
+	int str_size = strlen(str);
+	int i;
+
+	for (i = 0; i < str_size; i++) {
+		printf("%c", *(str + i));
+		fflush(stdout);
+		usleep(1000000);
+	}
+
+	printf("\n");
 	return 0;
 }
